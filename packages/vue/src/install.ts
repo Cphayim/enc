a46aa029d27e4092e9add96c4307b33f144c73da
Cphayim <i@cphayim.me>
2022-12-22 15:21:13 +0800

    feat(vue): quickly install tools and base hooks

--- packages/vue/src/install.ts
+++ packages/vue/src/install.ts
@@ -3,6 +3,9 @@ import type { App, Plugin } from 'vue'
 
 export type SFCWithInstall<T> = T & Plugin
 
+/**
+ * 为组件添加 install 方法
+ */
 export const withInstall = <T>(comp: T): SFCWithInstall<T> => {
   ;(comp as any).install = (app: App): void => {
     app.component((comp as any).name, comp as any)
@@ -10,6 +13,28 @@ export const withInstall = <T>(comp: T): SFCWithInstall<T> => {
   return comp as SFCWithInstall<T>
 }
 
-export function isComponent<T>(component: T): component is SFCWithInstall<T> {
-  return !!(component as any).install
+/**
+ * 判断传入的组件是否是 SFCWithInstall 类型
+ */
+export function isSFCWithInstall<T>(component: T): component is SFCWithInstall<T> {
+  return !!(component as any)?.install
+}
+
+/**
+ * 批量安装组件 app.use(xxx)
+ */
+export function usingSFCWithInstall(
+  app: App,
+  components:
+    | SFCWithInstall<unknown>
+    | SFCWithInstall<unknown>[]
+    | Record<string, SFCWithInstall<unknown>>,
+) {
+  if (isSFCWithInstall(components)) {
+    app.use(components)
+  } else if (Array.isArray(components)) {
+    components.forEach((component) => app.use(component))
+  } else {
+    Object.values(components).forEach((component) => app.use(component))
+  }
 }
