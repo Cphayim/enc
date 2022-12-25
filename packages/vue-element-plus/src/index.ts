e098c847a3ec7fc949c80eecf825348192196cb6
Cphayim <i@cphayim.me>
2022-12-26 02:00:25 +0800

    feat(vue,element-plus): `createEnc` function

--- packages/vue-element-plus/src/index.ts
+++ packages/vue-element-plus/src/index.ts
@@ -1,21 +1,30 @@
 import type { App, Plugin } from 'vue'
 
-import { usingSFCWithInstall } from '@cphayim-enc/vue'
+import { CreateEncOptions, usingSFCWithInstall } from '@cphayim-enc/vue'
 
+import './style.css'
 import * as componentMap from './components'
 import * as thirdPartyComponentMap from './components/third-party'
-import './style.css'
 
-const EncElementPlus: Plugin = {
-  install(app: App) {
-    // 先安装依赖的第三方组件
-    usingSFCWithInstall(app, thirdPartyComponentMap)
-    // 这个包中的组件
-    usingSFCWithInstall(app, componentMap)
-  },
+export const createEncElementPlus = ({
+  skipDepsInstall = false,
+  skipEncInstall = false,
+}: CreateEncOptions = {}): Plugin => {
+  return {
+    install(app: App) {
+      // 先安装依赖的第三方组件
+      if (!skipDepsInstall) {
+        usingSFCWithInstall(app, thirdPartyComponentMap)
+      }
+      // 这个包中的组件
+      if (!skipEncInstall) {
+        usingSFCWithInstall(app, componentMap)
+      }
+    },
+  }
 }
 
-export default EncElementPlus
+export default createEncElementPlus()
 
 export * from './upstream'
 export * from './hooks'
