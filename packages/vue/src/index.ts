61ea1714c5171db47f61a9868e88ca68181f1163
Cphayim <i@cphayim.me>
2022-12-26 02:00:25 +0800

    feat(vue,element-plus): `createEnc` function

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -2,3 +2,14 @@ export * from '@cphayim-enc/base'
 
 export * from './install'
 export * from './hooks'
+
+export type CreateEncOptions = {
+  /**
+   * 跳过依赖组件的安装
+   */
+  skipDepsInstall?: boolean
+  /**
+   * 跳过 enc 组件的安装
+   */
+  skipEncInstall?: boolean
+}
