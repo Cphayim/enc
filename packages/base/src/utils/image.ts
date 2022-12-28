aa1a3e3f43628466df653881a980f275fc36a563
Cphayim <i@cphayim.me>
2022-12-29 00:43:30 +0800

    feat(base): add `ImageUtils`, update `UploadFormItem`

---
+++ packages/base/src/utils/image.ts
@@ -0,0 +1,10 @@
+export class ImageUtils {
+  static getBase64ByFile(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = () => resolve(reader.result as string)
+      reader.onerror = (error) => reject(error)
+    })
+  }
+}
