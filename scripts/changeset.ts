c499e6550c2370fb91339cde5d1b0b62bb4eaa26
Cphayim <i@cphayim.me>
2022-12-30 14:38:20 +0800

    chore(script): changeset script

---
+++ scripts/changeset.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env tsx
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { execaCommandSync } from 'execa'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// add changes
+export async function changesetAdd() {
+  execaCommandSync('pnpm changeset add', { stdio: 'inherit', cwd: path.join(__dirname, '..') })
+}
+
+// release changes to version
+export async function changesetVersion() {
+  execaCommandSync('pnpm changeset version', { stdio: 'inherit', cwd: path.join(__dirname, '..') })
+  execaCommandSync('pnpm -r exec -- rimraf CHANGELOG.md', {
+    stdio: 'inherit',
+    cwd: path.join(__dirname, '..'),
+  })
+}
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  process.argv.includes('--version') ? changesetVersion() : changesetAdd()
+}
