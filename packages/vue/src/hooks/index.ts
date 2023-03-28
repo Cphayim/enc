b1b55ec7c2b37847800576d048044a047c2818fc
Cphayim <i@cphayim.me>
2023-03-28 18:08:20 +0800

    fix(vue): export `usePagination` hook (#71)

--- packages/vue/src/hooks/index.ts
+++ packages/vue/src/hooks/index.ts
@@ -2,6 +2,7 @@ export * from './use-event-lock'
 export * from './use-loading'
 export * from './use-confirm'
 export * from './use-emitter'
+export * from './use-pagination'
 
 export * from './use-form-data'
 export * from './use-form-items'
