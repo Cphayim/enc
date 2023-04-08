205c6c69557ac7e8559165f3b1a3224b972f5caa
Cphayim <i@cphayim.me>
2023-04-09 04:53:06 +0800

    feat(vue): add `EncTransition`

---
+++ packages/vue/src/components/transition/Transition.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Transition } from 'vue'
+
+defineOptions({ name: 'EncTransition' })
+</script>
+
+<template>
+  <Transition></Transition>
+</template>
+
+<style></style>
---
+++ packages/vue/src/components/transition/index.ts
@@ -0,0 +1,4 @@
+import { withInstall } from '../../install'
+import Transition from './Transition.vue'
+
+export const EncTransition = withInstall(Transition)
