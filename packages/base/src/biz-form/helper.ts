96461ba47eb31d1fb80f7ef4dae5fe3c831b3602
Cphayim <i@cphayim.me>
2023-05-25 12:09:56 +0800

    feat(base): `BizFormHelper` add matching rule

--- packages/base/src/biz-form/helper.ts
+++ packages/base/src/biz-form/helper.ts
@@ -1,12 +1,16 @@
 import type { FormItemUnion } from '../form'
 import type { BizFormItemUnion, BizPlaceHolderExtra, BizRealExtra } from './BizFormItemUnion'
 
+type FilterBizFormItemsMatchingRule = Partial<
+  Omit<BizPlaceHolderExtra, 'biz'> & Omit<BizRealExtra, 'biz'>
+>
+
 export class BizFormHelper {
   /**
    * 判断一个 `FormItemUnion` 是否是 `BizFormItemUnion`
    */
   static isBizFormItem(item: FormItemUnion): item is BizFormItemUnion {
-    return !!item.extra?.biz
+    return !!item.extra?.biz && !!item.extra?.bizName
   }
 
   /**
@@ -35,8 +39,14 @@ export class BizFormHelper {
   /**
    * 从 `FormItemUnion[]` 中找到所有 `BizFormItemUnion`
    */
-  static filterBizFormItems(items: FormItemUnion[]): BizFormItemUnion[] {
-    return items.filter(BizFormHelper.isBizFormItem)
+  static filterBizFormItems(
+    items: FormItemUnion[],
+    matchingRule?: FilterBizFormItemsMatchingRule,
+  ): BizFormItemUnion[] {
+    return BizFormHelper._matchingBizFormItems(
+      items.filter(BizFormHelper.isBizFormItem),
+      matchingRule,
+    )
   }
 
   /**
@@ -44,23 +54,34 @@ export class BizFormHelper {
    */
   static filterBizFormItemsPlaceholder(
     items: FormItemUnion[],
+    matchingRule?: FilterBizFormItemsMatchingRule,
   ): BizFormItemUnion<BizPlaceHolderExtra>[] {
-    return items.filter(BizFormHelper.isBizFormItemPlaceholder)
+    return BizFormHelper._matchingBizFormItems(
+      items.filter(BizFormHelper.isBizFormItemPlaceholder),
+      matchingRule,
+    )
   }
 
   /**
    * 从 `FormItemUnion[]` 中找到所有真实 `BizFormItemUnion`
    */
-  static filterBizFormItemsReal(items: FormItemUnion[]): BizFormItemUnion<BizRealExtra>[] {
-    return items.filter(BizFormHelper.isBizFormItemReal)
+  static filterBizFormItemsReal(
+    items: FormItemUnion[],
+    matchingRule?: FilterBizFormItemsMatchingRule,
+  ): BizFormItemUnion<BizRealExtra>[] {
+    return BizFormHelper._matchingBizFormItems(
+      items.filter(BizFormHelper.isBizFormItemReal),
+      matchingRule,
+    )
   }
 
   /**
-   * 查找 `formData` 中的业务数据
+   * 查找 `formData` 中的业务数据的值的分组，组名为 `bizKey`
+   * @deprecated
    * @param data 表单数据
    * @param items 表单项
    * @param bizName 业务名称
-   * @param bizFields 返回的 `bizValue` 数组，返回值将按照该数组的顺序排列
+   * @param bizFields 业务字段数组，返回值将按照该数组的顺序排列
    * @returns
    */
   static findBizValuesMap(
@@ -68,24 +89,40 @@ export class BizFormHelper {
     items: FormItemUnion[],
     bizName: string,
     bizFields: string[],
-  ) {
+  ): Record<string, any[]> {
     const bizItems = BizFormHelper.filterBizFormItemsReal(items)
-    const map: Record<string, any[]> = {}
+    const result: Record<string, any[]> = {}
 
     bizItems.forEach((item) => {
       const extra = item.extra!
       if (extra.bizName !== bizName) return
 
-      const list = map[extra.bizKey] ?? []
+      const list = result[extra.bizKey] ?? []
 
       bizFields.forEach((field, index) => {
         if (extra.bizField === field) {
           list[index] = data[item.name]
         }
       })
-      map[extra.bizKey] = list
+      result[extra.bizKey] = list
     })
 
-    return map
+    return result
+  }
+
+  /**
+   * 找到所有匹配规则的 `BizFormItemUnion`
+   * @param items
+   * @param matchingRule
+   * @returns
+   */
+  private static _matchingBizFormItems<T extends BizFormItemUnion>(
+    items: T[],
+    matchingRule?: FilterBizFormItemsMatchingRule,
+  ): T[] {
+    if (!matchingRule) return items
+    return items.filter((item) =>
+      Object.entries(matchingRule).every(([key, value]) => (item.extra as any)[key] === value),
+    )
   }
 }
