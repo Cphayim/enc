8d627869654679b64967b77054d1e99249ff838d
Cphayim <i@cphayim.me>
2023-06-03 14:53:16 +0800

    test(form-editor): add test

--- packages/extension-form-editor/src/__tests__/utils.spec.ts
+++ packages/extension-form-editor/src/__tests__/utils.spec.ts
@@ -1,5 +1,6 @@
+import { ENC_VERSION, FormItemUnion } from '@cphayim-enc/base'
 import { describe, expect, it } from 'vitest'
-import { isBizFeature, isPresetFeature } from '../utils'
+import { isBizFeature, isPresetFeature, markItemsCreatedByEditor } from '../utils'
 
 describe('utils', () => {
   it('should be able to determine whether it is a PresetFeature', () => {
@@ -18,5 +19,16 @@ describe('utils', () => {
     expect(isBizFeature({ bizName: 1, bizLabel: '1' })).toBe(false)
     expect(isBizFeature({ bizName: 1, bizLabel: '1', bizTransformer: {} })).toBe(false)
     expect(isBizFeature({ bizName: '1', bizLabel: '1', bizTransformer: {} })).toBe(true)
+  })
+
+  it('should be add a mark to items', () => {
+    const items: FormItemUnion[] = [
+      { name: 'a', label: 'A', type: 'custom' },
+      { name: 'b', label: 'B', type: 'custom' },
+    ]
+    const markedItems = markItemsCreatedByEditor(items)
+    markedItems.forEach((item) => {
+      expect(item).toMatchObject({ __CREATED_BY_ENC_FORM_EDITOR__: `v${ENC_VERSION}` })
+    })
   })
 })
