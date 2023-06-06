63ba4d46737a6e0f975e0ef2ccf26a64c458e75f
言蹊 <120307900+FE-avery@users.noreply.github.com>
2023-06-06 18:18:04 +0800

    fix(ex-form-editor): the right panel options set the label width (#116)

--- packages/extension-vue-form-editor/src/components/fieldset/Fieldset.vue
+++ packages/extension-vue-form-editor/src/components/fieldset/Fieldset.vue
@@ -8,7 +8,9 @@ const props = defineProps<{ title?: string }>()
   <fieldset
     class="enc-px-[10px] enc-pb-0 enc-border-solid enc-border enc-border-gray-300 enc-outline-none enc-rounded-[6px] enc-mb-[10px]"
   >
-    <legend class="enc-px-[8px] enc-text-[14px] enc-text-gray-500">
+    <legend
+      class="enc-flex enc-items-center enc-flex-nowrap enc-px-[8px] enc-text-[14px] enc-text-gray-500"
+    >
       <slot name="title">{{ props.title }}</slot>
     </legend>
     <slot />
